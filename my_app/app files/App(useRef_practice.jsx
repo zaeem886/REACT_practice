ceb64a68e6d1c13100f83ab29efd47a12ffc36b0
zaeem886 <zaeemch317@gmail.com>
2026-03-22 15:34:00 +0500

    useRef practice

---
+++ my_app/app files/App(useRef_practice.jsx
@@ -0,0 +1,53 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+function MyComponent(){
+
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    useEffect(() => {
+        console.log("COMPONENT RENDERED");
+    });
+
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "yellow";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "yellow";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "yellow";
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick1}>
+                Click me 1!
+            </button>
+            <input ref={inputRef1}/>
+
+            <button onClick={handleClick2}>
+                Click me 2!
+            </button>
+            <input ref={inputRef2}/>
+
+            <button onClick={handleClick3}>
+                Click me 3!
+            </button>
+            <input ref={inputRef3}/>
+        </div>
+    );
+}
+export default MyComponent;
